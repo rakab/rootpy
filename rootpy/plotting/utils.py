8d2852fa2ebdff60006be2723fd3fa797ee16be7
Bakar Chargeishvili <bakar.chargeishvili@gmx.de>
2019-01-05 22:39:23 +0400

    Improved func, can now contain errors of input plotables

--- rootpy/plotting/utils.py
+++ rootpy/plotting/utils.py
@@ -22,6 +22,7 @@ __all__ = [
     'canvases_with',
     'find_all_primitives',
     'tick_length_pixels',
+    'Compare',
 ]
 
 
@@ -480,7 +481,8 @@ class Compare(object):
          is free to choose any kind of mathematical operation, as long as it
          can be performed bin-wise on input plotables. To refer to some
          particular object from the input, use keys defined in plotables dict
-         as variable names (x0,x1,x2...).
+         as variable names (x0,x1,x2...), to access errors use
+         the letter 'd' in front of variable names (dx0,dx1,dx2...).
 
     errfunc : string, optional (default=None)
             A string defining function to use for the error calculation. If
@@ -508,18 +510,21 @@ class Compare(object):
 
     Example
     -------
-        result =  Compare({'x1':[h10,h11],'x2':[h20,h21,h22]}, '(x1-x2)/x1')
+        comp =  Compare({'x1':[h10,h11],'x2':[h20,h21,h22]}, '(x1-x2)/x1')
 
     This will calculate:
         (h10-h20)/h10, (h10-h21)/h10, (h10-h22)/h10, (h11-h20)/h11,
         (h11-h21)/h11 and (h11-h22)/h11
 
-    To acces the results involving h10 in the calculation, use:
-        result[h10]
+    To acces the results involving h10 in the calculation, run:
+        comp[h10]
 
     To further filter the results and get a list of objects involving h10 and
-    h21 together in the calculation, use:
-        result[h10, h21]
+    h21 together in the calculation, run:
+        comp[h10, h21]
+
+    To get the unfiltered result run:
+        comp.results
 
     """
 
@@ -607,7 +612,13 @@ class Compare(object):
                 raise KeyError("Key {0} must have a form of xN (N=0,1,2,...).".format(item))
 
         for item in self.symfunc.free_symbols:
-            if (str(item) in self.plotables.keys()) is False:
+            item = str(item)
+            if item[0] == 'd':
+                item = item[1:]
+            elif item[0] != 'x':
+                raise KeyError(("{0} has an incorrect form. Value from"
+                    " values from plotables should be indicated as xN, errors - as dxN (N=0,1,2,...)".format(item)))
+            if (item in self.plotables.keys()) is False:
                 raise KeyError("{0} is being used in func, but not found in plotable keys.".format(str(item)))
 
         if (errfunc is not None) and (not isinstance(errfunc,str)):
@@ -623,7 +634,7 @@ class Compare(object):
                     item = item[1:]
                 elif item[0] != 'x':
                     raise KeyError(("{0} has an incorrect form. Value from"
-                        " plotable should be indicated as xN, error - as dxN (N=0,1,2,...)".format(item)))
+                        " values from plotables should be indicated as xN, errors - as dxN (N=0,1,2,...)".format(item)))
                 if (item in self.plotables.keys()) is False:
                     raise KeyError("{0} is being used in errfunc, but not found in plotable keys.".format(item))
 
@@ -631,16 +642,23 @@ class Compare(object):
             #Calculate standard error from func, assuming variables are independent
             self.errfunc = self.sympy.sympify(0)
             for item in self.symfunc.free_symbols:
-                d = (self.sympy.diff(self.symfunc,item)*self.sympy.var('d'+str(item)))**2
+                if str(item)[0] == 'd':
+                    #If errors from plotables are used in func, treat them as a
+                    #const term during error calculation, i.e. error has no
+                    #error itself
+                    d = 0
+                else:
+                    d = (self.sympy.diff(self.symfunc,item)*self.sympy.var('d'+str(item)))**2
                 self.errfunc += d
             self.errfunc = self.errfunc**0.5
 
         #List of output plotables
-        self.result = []
+        self.results = []
         #Resulting plotable will have the same style as input plotable with the
         #biggest N in xN keys of input plotables
         self.symkeys = self.symfunc.free_symbols
-        self.keys = [int(str(key)[1:]) for key in self.symkeys]
+        keys = [str(key) for k in self.symkeys]
+        self.keys = [int(re.findall(r"\d+",k)[0]) for k in keys]
         self.keys.sort()
 
         if keytoclone is None:
@@ -690,7 +708,7 @@ class Compare(object):
             #Perform actual evaluation of func and errorfunc
             self.evaluate(comb,result)
             #Save result in the output list
-            self.result.append(result)
+            self.results.append(result)
 
 
     def evaluate(self, comb, result):
@@ -703,7 +721,10 @@ class Compare(object):
                 subs_func = {}
                 subs_errfunc = {}
                 for key in self.symkeys:
-                    subs_func[key] = comb[str(key)][i].value
+                    if str(key)[0] != 'd':
+                        subs_func[key] = comb[str(key)][i].value
+                    else:
+                        subs_func[key] = comb[str(key)[1:]][i].error
                 for key in self.errfunc.free_symbols:
                     if str(key)[0] != 'd':
                         subs_errfunc[key] = comb[str(key)][i].value
@@ -717,7 +738,12 @@ class Compare(object):
                 subs_errfunc_hi = {}
                 subs_errfunc_low = {}
                 for key in self.symkeys:
-                    subs_func[key] = comb[str(key)][i].y.value
+                    if str(key)[0] != 'd':
+                        subs_func[key] = comb[str(key)][i].y.value
+                    else:
+                        subs_func[key] = 0.5*(comb[str(key)[1:]][i].y.error_low+comb[str(key)[1:]][i].y.error_hi)
+                        log.warning(
+                                "Errors from Graph are used in func calculation, considering mean of low and hi errors")
                 for key in self.errfunc.free_symbols:
                     if str(key)[0] != 'd':
                         subs_errfunc_low[key] = comb[str(key)][i].y.value
@@ -741,5 +767,5 @@ class Compare(object):
             result[h1,h2,h3]
         """
         if isinstance(item,tuple) or isinstance(item,list):
-            return [obj for obj in self.result if set(item).issubset(set(obj.parents))]
-        return [obj for obj in self.result if item in obj.parents]
+            return [obj for obj in self.results if set(item).issubset(set(obj.parents))]
+        return [obj for obj in self.results if item in obj.parents]
